246e13a54210e103518c6f803eb7a14b434808a1
NesreenKhalid <38976296+NesreenKhalid@users.noreply.github.com>
2020-01-15 09:25:22 +0200

    number of cars decreased and resize them

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Car = function(x, y) {
     this.sprite = 'images/car.png';
     this.x=x;
     this.y=y;
+    this.width = 200;
+    this.hight = 250;
    
 
 };
@@ -51,8 +53,9 @@ Car.prototype.update = function(dt) {
     }
     else {
     this.x=-90;
-        if(character.score>=5) {
-            this.speed = randomIntFromInterval(130,500);
+        if(character.score>=2) {
+            this.speed = randomIntFromInterval(200,500);
+            goToLevel(2);
         }
         else if(character.score>=8) {
             this.speed = randomIntFromInterval(200,800);
@@ -65,31 +68,10 @@ detectCollision(character,this)
   
 };
 
-function detectCollision(char , enemy){
-    var left1 = char.x ;
-    var up1 = char.y;
-
-    var right2 = enemy.x ;
-    var down2 =  enemy.y ;
-
-    
-    
-
-    if( (left1-70 < right2 && right2<left1+40 && up1-30 < down2 && down2 < up1+30) ) {
-    character.score=0;
-        document.getElementById("score").innerHTML = character.score;
-        character.reset();
-        console.log("collision !!");}
- 
-
-
-}
-
 
 Car.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y, this.width, this.hight);
 };
-
 
 var character = function() {
     this.sprite='images/hero1.png';
@@ -117,31 +99,29 @@ function moveDown(){
     
 }
 
-  
-  
 character.prototype.update = function(dt) {console.log(this.y)
     if(this.y==60) {
         
         this.score+=1;
         document.getElementById("score").innerHTML = this.score;
-
+        
         this.reset()
     }
     if( this.y==410)
     window.scrollTo( 0, this.y - character.step );
     if( this.y>750)
     window.scrollTo( 0, this.y - character.step );
-   
+    
 };
 
 character.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     
-
+    
 };
 
 character.prototype.handleInput = function(key) {
-
+    
     if(key=="up" && this.y>30) {
         moveUp();
     }
@@ -154,7 +134,7 @@ character.prototype.handleInput = function(key) {
     else if(key=='left' && this.x>10) {
         moveLeft();
     }
-   
+    
 };
 
 character.prototype.reset = function() {
@@ -164,28 +144,25 @@ character.prototype.reset = function() {
 };
 
 
-
 // Now instantiate your objects.
-var a = new Car(-10, 65);
-var b = new Car(-50, 145);
-var c = new Car(-100, 225);
-var d = new Car(-200, 65);
-var e = new Car(-400, 145);
-var f = new Car(-600, 225);
-var g= new Car(-320, 340);
-var h= new Car(-20, 290);
-var i= new Car(-60, 490);
-var j= new Car(-220, 590);
-var k= new Car(-200, 590);
-var Beetle1= new Beetle(-10,70);
-var Beetle2= new Beetle(-40,350);
-var Beetle3= new Beetle(-80,65);
-var Beetle4= new Beetle(-150,410);
-var Beetle5= new Beetle(70,500);
-allEnemies = [a,b,c,d,e,f,g,h,i,j,k,Beetle1,Beetle2,Beetle3,Beetle4,Beetle5];
+var a = new Car(-1000, 70);
+var b = new Car(-1000, 240);
+var c = new Car(-1000, 325);
+var d = new Car(-20, 500);
+// var e = new Car(-400, 145);
+// var f = new Car(-600, 225);
+// var g= new Car(-320, 340);
+// var h= new Car(-20, 290);
+// var i= new Car(-60, 490);
+// var j= new Car(-220, 590);
+// var k= new Car(-200, 590);
+// var Beetle1= new Beetle(-10,70);
+// var Beetle2= new Beetle(-40,350);
+// var Beetle3= new Beetle(-80,65);
+// var Beetle4= new Beetle(-150,410);
+// var Beetle5= new Beetle(70,500);
+allEnemies = [a,b,c,d];//,e,f,g,h,i,j,k,Beetle1,Beetle2,Beetle3,Beetle4,Beetle5];
 var character = new character();
-
-
 
 
 function randomIntFromInterval(min,max)
@@ -204,9 +181,25 @@ document.addEventListener('keydown', function(e) {
     character.handleInput(allowedKeys[e.keyCode]);
 });
 
+function detectCollision(char , enemy){
+    var left1 = char.x ;
+    var up1 = char.y;
+
+    var right2 = enemy.x ;
+    var down2 =  enemy.y ;
+
+    
+    
+
+    if( (left1-70 < right2 && right2<left1+40 && up1-30 < down2 && down2 < up1+30) ) {
+    character.score=0;
+        document.getElementById("score").innerHTML = character.score;
+        character.reset();
+        console.log("collision !!");}
+
+}
 
 
-
-
-
-
+function goToLevel(num){
+    
+}
